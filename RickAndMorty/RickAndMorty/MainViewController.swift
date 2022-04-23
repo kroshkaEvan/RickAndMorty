2c44d287aa9dcbb046c2903f25cc8754069c1373
Эван Крошкин <>
2022-04-23 15:44:18 +0300

    Initial project setup

---
+++ RickAndMorty/RickAndMorty/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  RickAndMorty
+//
+//  Created by Эван Крошкин on 23.04.22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
